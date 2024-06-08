13fc6eb127352e1bca49e8a89c32d3db4d2f3706
felixmacaspac <fmcmacaspac@protonmail.com>
2024-06-08 12:23:48 +0800

    feat: implement footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,5 +1,27 @@
-<footer class="footer hidden"> 
-  <div class="container">
-    Footer
+<footer class="footer bg-[#DCD5BC] py-10"> 
+  <div class="container mx-auto">
+    <div class="flex justify-between items-center">
+      <div>
+        <img src="/public/img/logo/logo-moondust.png" alt="moondust logo">
+        <p class="text-lg mt-2 text-black/80">© moondust. 2024 all rights reserved.</p>
+      </div>
+      <ul class="flex flex-row items-center gap-8">
+          <li>
+          <a href="/">
+            <img src="public/img/icons/tiktok.svg" alt="">
+          </a>
+          </li>
+          <li>
+          <a href="/">
+            <img src="public/img/icons/instagram.svg" alt="">
+          </a>
+          </li>
+          <li>
+          <a href="/">
+            <img src="public/img/icons/facebook.svg" alt="">
+          </a>
+          </li>                    
+      </ul>
+    </div>
   </div>
 </footer>
